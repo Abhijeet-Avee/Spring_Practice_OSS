2ee8e817a36bb3b1bd9185a3de6772580d03bc70
Abhijeet Kumar <VE00YM318@YMI.COM>
2022-02-20 23:34:50 +0530

    Spring assignment

--- 17.bankapp_annotation_database_javaconfig/src/main/java/com/bankapp/model/dao/AccountDaoImplJdbc.java
+++ 17.bankapp_annotation_database_javaconfig/src/main/java/com/bankapp/model/dao/AccountDaoImplJdbc.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 @Repository
@@ -15,6 +16,7 @@ public class AccountDaoImplJdbc implements AccountDao {
 
 	private DataSource dataSource;
 
+	@Autowired
 	public AccountDaoImplJdbc(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
